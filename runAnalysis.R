ebfeb638b1a376c44e9b4413c2dd5e68906fc34a
bawdiest <makarov-mikhail@bawdiest-MacBook-Air.local>
2015-03-17 20:08:51 +0100

    Define the steps of Process
    Define the steps of Process as Sections in R File

---
+++ runAnalysis.R
@@ -0,0 +1,11 @@
+
+# Get List of Items (Words) and provide ID --------------------------------
+
+
+# Convert Sentence into Vector (Basket) with Words (Items) ----------------
+# Shingle
+
+
+# Calculate Jacard Similarity ---------------------------------------------
+# Make a dot product of a matrix to sum up the words which are in both sentences
+
