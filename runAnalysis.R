222916b8488130ef60927a8504636997d266516e
bawdiest <makarov-mikhail@bawdiest-MacBook-Air.local>
2015-03-18 14:15:44 +0100

    Replace First Word
    The first word in sentences is the row number. It should be removed, as
    it is different for every sentence.

--- runAnalysis.R
+++ runAnalysis.R
@@ -1,4 +1,18 @@
 
+sourceData <- read.delim("data/sentencesExp.txt", header=FALSE);
+#// replace first word (row Number)
+
+replaceFirstWord <- function(x) {
+  r <- as.character(x);
+  t <- strsplit(r, " ", fixed=TRUE);
+  t <- t[[1]][1];
+  x <- sub(t, "", x)
+  x <- sub("^ ", "", x)
+  x
+  };
+
+sourceData <- data.frame(apply(sourceData, FUN = replaceFirstWord, MARGIN = c(1)));
+
 # Get List of Items (Words) and provide ID --------------------------------
 
 
